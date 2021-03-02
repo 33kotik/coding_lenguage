32effe66445e908204359339e8f617f150a57351
alex <aleshka59000@gmail.com>
2021-03-03 00:11:13 +0300

    change

--- 1/main.cpp
+++ 1/main.cpp
@@ -3,35 +3,70 @@
 #include <math.h>
 
 using namespace std;
+
 class equal1 {
 public:
     double a;
     double b;
     double c;
 
-    double d(){
-        return(b*b-4*a*c);
+    double d() {
+        return (b * b - 4 * a * c);
     };
 
-    pair<double, double> slowe (){
-        if ( d()>=0) {
+    pair<double, double> slowe() {
+        if (d() >= 0) {
             pair<double, double> ans;
 //            cerr<<(-b - sqrt(d())) / (2 * a)<<endl;
             ans.first = (-b - sqrt(d())) / (2 * a);
             ans.second = (-b + sqrt(d())) / (2 * a);
             return (ans);
+        } else {
+            cerr << "мнимые корни ,ведиете новые кофициенты  a,b,c" << endl;
+            cin >> a >> b >> c;
+            return (slowe());
         }
-        else {
-            cerr << "мнимые корни ведиете новые кофициенты  a,b,c" << endl;
-            cin>>a>>b>>c;
-        }
+    }
+
+    void read_coefficient() {
+        cout << "введите коэфиценты a, b, c при x^2,x,x^0 в ax^2+bx+c=0 " << endl;
+        cin >> a >> b >> c;
+    }
+
+    void write_answer() {
+        pair<double, double> ans = slowe();
+        cout << " x1=" << ans.first << endl << " x2=" << ans.second << endl;
+    }
+
+    void write_equal() {
+        string ans = to_string(a) + "x^2 + " + to_string(b) + "x + " + to_string(c);
+        cout << ans << endl;
     }
 };
 
 int main() {
-    cout << "Hello, World! введите коэфиценты a, b, c при x^2,x,x^0 в ax^2+bx+c=0 "  << endl;
+
     equal1 my;
-    cin>>my.a>>my.b>>my.c;
-    cout<<"anwer x1="<<my.slowe().first<<endl<<" x2="<<my.slowe().second<<endl;
+    while (true) {
+        int c = 0;
+        cout << "press 1 to read coefficient" << endl
+             << "press 2 to write answer" << endl
+             << "press 3 to write equal" << endl
+             << "press 4 to stop" << endl;
+        cin >> c;
+        if (c == 1) {
+            my.read_coefficient();
+        }
+
+        if (c == 2) {
+            my.write_answer();
+        }
+        if (c == 3) {
+            my.write_equal();
+        }
+        if (c == 4) {
+            break;
+        }
+    }
     return 0;
 }
